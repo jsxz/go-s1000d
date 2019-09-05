270bb9c27899003e17ce7c068f807db72deeefe1
anjunact <anjunact@qq.com>
2019-09-05 10:17:31 +0800

    add style

--- main.go
+++ main.go
@@ -6,47 +6,46 @@ import (
 	"github.com/jsxz/go-s1000d/db"
 	"io/ioutil"
 	"regexp"
-	"strings"
 )
 
 func main() {
 	folder := "xsd/4.2"
 	files := listFile(folder)
-	for _,f := range files{
+	for _, f := range files {
 		//if strings.HasSuffix(f,"proced.xsd"){
-			fmt.Println(f)
-			s := readToString(f)
-			run(s,`<xs:element\sname="(\w+)"`)
+		fmt.Println(f)
+		s := readToString(f)
+		run(s, `<xs:element\sname="(\w+)"`)
 		//}
 	}
 }
-func run(s ,pattern  string )  {
+func run(s, pattern string) {
 	reg := regexp.MustCompile(pattern)
 	submatch := reg.FindAllSubmatch([]byte(s), -1)
 	var items []string
-	for _,v:= range  submatch{
-		for _,v1:=range v{
-			s1:=string(v1)
-			if !strings.HasPrefix(s1,"<xs"){
-				//fmt.Println(s1)
-				items = append(items,s1)
-				db.ElementAdd(s1,4)
+	for _, v := range submatch {
+		for i, v1 := range v {
+			s1 := string(v1)
+			if i == 1 {
+				fmt.Println(s1)
+				items = append(items, s1)
+				db.ElementAdd(s1, 4)
 			}
 		}
 	}
 	fmt.Println(items)
 	fmt.Println(len(items))
 }
-func readToString(f string) string  {
+func readToString(f string) string {
 	bytes, e := ioutil.ReadFile(f)
 	common.CheckError(e)
 	return string((bytes))
 }
 
-func listFile(folder string)[]string{
+func listFile(folder string) []string {
 	var res []string
 	files, _ := ioutil.ReadDir(folder) //specify the current dir
-	for _,file := range files {
+	for _, file := range files {
 		if file.IsDir() {
 			listFile(folder + "/" + file.Name())
 		} else {
@@ -54,5 +53,5 @@ func listFile(folder string)[]string{
 			res = append(res, folder+"/"+file.Name())
 		}
 	}
-	return  res
+	return res
 }
