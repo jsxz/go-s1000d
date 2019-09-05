bfb28499101da0f1ab62e097f294ad3ddbc43e4f
anjunact <anjunact@qq.com>
2019-09-05 09:51:56 +0800

    add style

--- db/mysql.go
+++ db/mysql.go
@@ -2,12 +2,14 @@ package db
 
 import (
 	"fmt"
-	_ `github.com/go-sql-driver/mysql`
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/jsxz/go-s1000d/common"
 	"github.com/jsxz/go-s1000d/models"
 )
+
 var DB *sqlx.DB
+
 func init() {
 	var err error
 	//DB, err = sqlx.Open(`mysql`, `root:root@tcp(127.0.0.1:3306)/go-s1000d?charset=utf8&parseTime=true`)
@@ -17,30 +19,30 @@ func init() {
 		panic(err)
 	}
 }
-func ElementAdd(name string,version int) bool  {
-	_, e := ElementOne(name)
+func ElementAdd(name string, version int) bool {
+	obj, e := ElementOne(name)
 	common.CheckError(e)
-	if e != nil{
-		fmt.Println(name+"已存在")
+	if e != nil || name == obj.Name {
+		fmt.Println(name + "已存在")
 		return false
 	}
-	_,err:=DB.Exec("insert into `element` (`name`,`version`) values(?,?)",name,version)
+
+	_, err := DB.Exec("insert into `element` (`name`,`version`) values(?,?)", name, version)
 	common.CheckError(err)
 	return true
 }
-func ElementAll() ([]models.Element,error)  {
+func ElementAll() ([]models.Element, error) {
 	mods := make([]models.Element, 0)
 	err := DB.Select(&mods, "select * from `element` sort by id asc")
 	return mods, err
 }
-func ElementOne(name string)( models.Element,error) {
+func ElementOne(name string) (models.Element, error) {
 	mods := make([]models.Element, 0)
-	err :=DB.Select(&mods,"select * from `element` where name=?",name)
+	err := DB.Select(&mods, "select * from `element` where name=?", name)
 	common.CheckError(err)
-	if len(mods)>0{
-		return  mods[0] ,err
-	}else{
-		return models.Element{},err
+	if len(mods) > 0 {
+		return mods[0], err
+	} else {
+		return models.Element{}, err
 	}
 }
-
